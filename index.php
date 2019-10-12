a01e0c4210630467b038ad98d1f6325487306b0d
lalysingh <lalysingh1996@hotmail.fr>
2019-10-12 21:49:13 +0200

    email

--- index.php
+++ index.php
@@ -22,9 +22,9 @@ function error($message, $color){
 	return "<small class='$color'> $message </small>";
 }
 
-$subject = "Merci pour votre email";
-$textmsg = "Bonjour". $okfirstname . "merci pour votre demande, nous vous repondrons treeees vite";
-$headers = "hello@poulette.cotcot";
+$subject = "Thanks for your email";
+$textmsg = "Hello". $filteredfirstname . "we will get back to you today";
+$headers = "hello@poulette.be";
 
 mail($sanitizeemail,$subject,$textmsg,$headers);
 
@@ -53,44 +53,42 @@ mail($sanitizeemail,$subject,$textmsg,$headers);
 					</div>
 					<div class="row ml-2">
 						<div class="col-12 col-md-5">
-							<label for="NomDeFam" class="text-white">Your last name</label>
+							<label for="NomDeFam" class="text-white" title="Your email please">Your last name</label>
 							<br/>
-							<input type="text" name="NomDeFam" id="NomDeFam" value="" maxlength="20" title="Your surname please"/>
+							<input type="text" name="NomDeFam" id="NomDeFam" value="" maxlength="20" title="Your surname please" placeholder="BestCoachs"/>
 							<?php
 							// check if the input last name equals not the filtered lastname from special chars
 							if($lastname != specialchar($lastname)){
-								$oklastname = $lastname ;
 								echo error("Please enter only letters", "error");
 							//stores the lastname in variable
 							} else {
-								$getlastname = $lastname;
+								$filteredlastname = $lastname;
 							}
 							if (empty($lastname) && (isset($submityou))){
-								echo error("Please type your lastname", "error");
+								echo error("Please type your lastname", "info");
 							}
 							?>
 						</div>
 						<div class="col-12 col-md-5 offset-md-2">
 							<label for="firstname" class="text-white" title="your firstname below please">Firstname</label>
 							<br/>
-							<input type="text" name="firstnamee" id="firstname" value="" maxlength="10" title="Your firstname please"/>
+							<input type="text" name="firstnamee" id="firstname" value="" maxlength="10" title="Your firstname please" placeholder="Marly"/>
 							<?php
 							//check for special chars in firstname
 							if($firstname != specialchar($firstname)){
-								$okfirstname = $firstname;
+								$filteredfirstname = $firstname;
 								echo error("Please enter only letters", "error");
 							} else {
 								$getfirstname = $firstname;
 							}
 							if (empty($firstname)  && (isset($submityou))){
-								echo error("Please type your firstname", "error");
+								echo error("Please type your firstname", "info");
 							}
 							?>
 						</div>
 					</div>
-					<div class="row ml-2">
-						<div class="col-12 col-md-5 mt-2">
-							<div>
+					<div class="row ml-2 mt-2">
+							<div class="col-12 col-md-5 mt-2">
 								<label for="fem" class="text-white" title="female button">Woman</label>
 								<input type="radio" name="gender" id="fem" value="fem" title="are you a woman?"/>
 								<br/>
@@ -102,14 +100,13 @@ mail($sanitizeemail,$subject,$textmsg,$headers);
 								}
 								?>
 							</div>
-						</div>
 						<div class="col-md-2">
 						</div>
-						<div class="row ml-2">
-							<div class="col-12 offset-md-1">
+						<div class="row mt-2">
+							<div class="col-12 offset-1">
 								<label for="country" class="text-white">Country</label>
 							</div>
-							<div class="col-12 offset-md-1">
+							<div class="col-12 offset-1">
 								<select name="yourcountry" id="country" title="which country are your from?">
 									<option value="Belgium" class="text-white" title="Belgium">Belgium</option>
 									<option value="France" class="text-white" title="France">France</option>
@@ -122,15 +119,15 @@ mail($sanitizeemail,$subject,$textmsg,$headers);
 								<label for="cmoncontact" class="text-white" title="your email below please">E-mail</label>
 							</div>
 							<div class="col-12">
-								<input  name="cmoncontact" id="cmoncontact" class="col-12" type="email" title="Your email please"/>
+								<input  type="text" name="cmoncontact" id="cmoncontact" class="col-12" title="Your email please" placeholder="egg@poulette.cotcot"/>
 								<?php
 									if($email != $sanitizeemail) {
 									echo error("Please enter a valid email", "error");
 									} else {
-										$getfirstname = $firstname;
+										$filteredeamil = $email;
 									}
 									if (isset($submityou) && empty($email)){
-										echo error("Please type an email", "error");
+										echo error("This needs to be filled , type an email please", "info");
 									}
 								?>
 							</div>
@@ -138,15 +135,15 @@ mail($sanitizeemail,$subject,$textmsg,$headers);
 						<label for="subject" class="text-white">Rasberry type</label>
 						<br/>
 						<select name="rasberrytype" id="subject" title="which type of rasberry?">
-							<option value="simple" title="simple rasberry">simple rasberry</option>
-							<option value="intermediate" title="more complex rasberry"> rasberry+</option>
-							<option value="advanced" title="optimised rasberry">rasberry++</option>
+							<option value="simple" title="starter kit">kit starter</option>
+							<option value="intermediate" title="more complex rasberry kit"> kit intermediate</option>
+							<option value="advanced" title="for legends rasberry kit">kit intermediate+ </option>
 							<option value="other" selected title="other">Other</option>
 						</select>
 						</div>
 						<div class="col-12 mt-2">
 							<label for="yourmsg" class="text-white" title="your message below please">Your message</label>
-							<textarea name="msgyours" id=yourmsg rows="5" cols="10" class="col-12" title="your message here"></textarea>
+							<textarea type ="text" name="msgyours" id=yourmsg rows="5" cols="10" class="col-12" title="your message here" placeholder="Dear poulette, this is a message to tell you that..."></textarea>
 							<?php
 							if ($msg != $filtermsg){
 								echo error("Are you trying to do something?","error");
@@ -154,12 +151,12 @@ mail($sanitizeemail,$subject,$textmsg,$headers);
 								$getmessage = $msg;
 							}
 							if (empty($msg) && (isset($submityou))){
-								echo error("Type a message please", "error");
+								echo error("This needs to be filled, type a message please", "info");
 							}
 							?>
 						</div>
 						<div class="col text-center">
-							<button type="Submit" class="btn colorbuton mt-2 mb-2" name="submit">Submit</button>
+							<button type="Submit" class="btn btnstyle mt-2 mb-2" name="submit">Submit</button>
 						<div>
 				</form>
 			</div>
