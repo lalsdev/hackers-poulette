7a787abd7fc8898c023dc14167199e461fc2e25e
lalysingh <lalysingh1996@hotmail.fr>
2019-10-12 21:58:50 +0200

    bug email

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ function error($message, $color){
 }
 
 $subject = "Thanks for your email";
-$textmsg = "Hello". $filteredfirstname . "we will get back to you today";
+$textmsg = "Hello". $firstname . "we will get back to you today";
 $headers = "hello@poulette.be";
 
 mail($sanitizeemail,$subject,$textmsg,$headers);
